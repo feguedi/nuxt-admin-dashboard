31f084860c243c32cc5703d1134817ee4749eb82
Ixe Fernández (Vagrant Debian) <feguedi@yahoo.com.mx>
2023-12-14 00:46:05 -0600

    feat: Add custom scrollbar styles and font
    This change adds custom scrollbar styles and a new font, 'Source Sans
    3', to the application. The custom scrollbar styles enhance the user
    experience by providing a more visually appealing and consistent look.
    The new font improves readability and overall aesthetics. Additionally,
    the necessary changes have been made to the nuxt.config.ts file to
    include the font stylesheet.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,19 @@ import path from 'path';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+        {
+          href: 'https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@400;600;700&display=swap',
+          rel: 'stylesheet',
+        },
+      ],
+    },
+  },
+  css: ['~/assets/styles/main.css'],
   alias: {
     '@': path.resolve(__dirname, '/'),
   },
